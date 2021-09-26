09efd0b612f8d0b06e54578197b5328bd74d5738
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-09-26 15:13:18 +0600

    some design issues solved

--- src/components/Contstatus/Contstatus.js
+++ src/components/Contstatus/Contstatus.js
@@ -9,16 +9,17 @@ const Contstatus = (props) => {
     }
     let count = 100;
     return (
-        <div className='text-center'>
+        <div className='text-center bg-light shadow-sm border rounded'>
             <h2 className='text-info'>Board</h2>
             <h5>Top Contributors: {subscribers.length}</h5>
             <p>Total Contribution: ${total}</p>
+            <h6>Contributors Names</h6>
             <ul className="list-group list-group-numbered">
                 {   
                     // mapping names array for showing name of the companies. 
                     props.names.map(name => <li
                         key={count++}
-                        className="list-group-item"
+                        className="list-group-item bg-light"
                     >{name}</li>)
                 }
             </ul>
